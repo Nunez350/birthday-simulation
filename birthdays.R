20d7aafba090760e480dbebad2b8c0d3d9ddb76a
Roy NUNEZ <roynunez@Roys-Air.home>
2017-02-28 09:33:08 -0500

    Feb 28 2017

--- birthdays.R
+++ birthdays.R
@@ -1,5 +1,5 @@
 # Random Birthday Simulation
-# What is the probability that NONE of the N people in a room shares a birthday
+# What is the probability that NONE of the N people in a room shares a birthday?
 
 N <- 0
 samples <- 0;
@@ -9,17 +9,17 @@ for(j in 10:100){
     counting.no.dups <- 0;
     test <- for(i in 1:1000){
     bdays <- sample(seq(as.Date('1990/01/01'), as.Date('1990/12/31'), by="day"), N, replace=T)
-    dups <- duplicated(bdays, incomparables = FALSE)
-    ch <-length(which(dups == TRUE))
-    if(ch==0){
+    dups <- duplicated(bdays, incomparables = FALSE) #returns duplicates
+    ch <-length(which(dups == TRUE)) #returns number of duplicates
+    if(ch==0){ #if there are no dupicates the variable is incremented by one
       counting.no.dups <- counting.no.dups +1
     }
-    fine <- (counting.no.dups/1000)
+    fine <- (counting.no.dups/1000) 
     }
  print(N)
  print(fine)
  N <- N + 1
- samples[[j]] <- N
+ samples[[j]] <- N #variables are stored consecutively
  prob.nodub[[j]] <- fine
 
 }
